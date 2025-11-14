55694cb89b48ea73e64cc1bfb45c6053f70d2f25
junechol ahn <junechol.ahn@gmail.com>
2025-11-15 08:54:53 +0900

    saves hashed password

--- models.py
+++ models.py
@@ -1,14 +1,26 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 
 class Todos(Base):
-  __tablename__ = 'todos'
+    __tablename__ = "todos"
 
-  id = mapped_column(Integer, primary_key=True, index=True)
-  title = mapped_column(String)
-  description = mapped_column(String)
-  priority = mapped_column(Integer)
-  complete = mapped_column(Boolean, default=False)
-
-
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
