89d4f78c8c81e361fe4fa2a091cb68893e7c57e0
junechol ahn <junechol.ahn@gmail.com>
2025-11-17 22:58:32 +0900

    added alembic revision

--- routers/auth.py
+++ routers/auth.py
@@ -28,6 +28,7 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+    phone_number: str
 
 class Token(BaseModel):
     access_token: str
@@ -88,6 +89,7 @@ async def create_user(db: db_dependency,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
         hashed_password=crypt_context.hash(create_user_request.password),
+        phone_number=create_user_request.phone_number,
         is_active=True
     )
 
--- routers/users.py
+++ routers/users.py
@@ -54,3 +54,19 @@ async def change_password(user:user_dependency,
     
     db.add(user_model)
     db.commit()
+
+@router.put('/phone_number', status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user:user_dependency,
+                          db:db_dependency,
+                          new_phone_number: str):
+    if user is None:
+        raise HTTPException(status_code=401, detail="authentication failed")
+    user_model = db.query(Users) \
+                   .filter(Users.id == user.get('id')).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="user not found")
+    
+    user_model.phone_number = new_phone_number # type: ignore
+    
+    db.add(user_model)
+    db.commit()
